0304225f7f7850c5e9c4a52cecbb3ab91027e40d
unknown <macheteviernes13@gmail.com>
2026-01-29 20:59:14 -0400

    initial commit

---
+++ src/components/BudgetForm.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react"
+
+export default function BudgetForm() {
+
+    const [budget, setBudget ] = useState(0)
+
+    const handleChange = (e : React.ChangeEvent<HTMLInputElement>) => {
+        setBudget(e.target.valueAsNumber)
+       
+
+    }
+
+
+
+    return (
+        <form className="space-y-5">
+            <div className="flex flex-col space-y-5" >
+                <label htmlFor="budget" className="text-4xl text-blue-600 font-bold text-center ">
+                    Definir presupuesto
+                </label>
+                <input type="number" 
+                id="budget"
+                className="w-full bg-white border bordger-gray-200 p-2"
+                placeholder="Define tu presupuesto"
+                name="budget"
+                value={budget}
+                onChange={handleChange}
+                />
+
+            </div>
+
+            <input
+             type="submit" 
+             value="definir presupuesto"
+             className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-black uppercase"
+            />
+        </form>
+    )
+}
